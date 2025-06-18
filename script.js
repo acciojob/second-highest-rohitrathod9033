f7a2b7741d57bab503f5ec334f6e3509099e0285
Rohit Rathod <86867651+rohitrathod9033@users.noreply.github.com>
2025-06-18 12:24:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+function secondHighest(arr) {
+	arr = arr.map(Number); // Convert strings to numbers
+
+	let max = -Infinity;
+	let second = -Infinity;
+
+	for (let num of arr) {
+		if (num > max) {
+			second = max;
+			max = num;
+		} else if (num < max && num > second) {
+			second = num;
+		}
+	}
+
+	return second;
+}
+
+function Main() {
+	var n = prompt("Enter the number of elements");
+	var arr = [];
+	for (var i = 0; i < n; i++) {
+		arr[i] = prompt("Enter element " + (i+1));
+	}
+	alert(secondHighest(arr));
+}
+Main();
